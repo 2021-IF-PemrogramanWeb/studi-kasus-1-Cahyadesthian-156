3c078b3f892e63329ed62e098c057e1bd9387447
Cahyadesthian-156 <widigda.19051@mhs.its.ac.id>
2021-11-04 15:21:22 +0700

    informasi login

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Studi Kasus 1</title>
+  </head>
+  <body>
+    
+
+    <div class="container pt-5">
+
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+            <!-- card1 -->
+            <div class="col">
+                <div class="card">
+                <img src="photo/agniasari.jpg" class="card-img-top" alt="user1">
+                <div class="card-body">
+                    <h5 class="card-title">Username : agniasari</h5>
+                    <p class="card-text">Password : agniasariPass </p>
+                </div>
+                </div>
+            </div>
+
+            <!-- card 2 -->
+            <div class="col">
+                <div class="card">
+                <img src="photo/kencana.jpg" class="card-img-top" alt="user2">
+                <div class="card-body">
+                    <h5 class="card-title">Username : kencana</h5>
+                    <p class="card-text">Password : kencanaPass</p>
+                </div>
+                </div>
+            </div>
+
+            <!-- card 3 -->
+            <div class="col">
+                <div class="card">
+                <img src="photo/sasmaya.jpg" class="card-img-top" alt="user3">
+                <div class="card-body">
+                    <h5 class="card-title">Username : sasmaya</h5>
+                    <p class="card-text">Password : sasmayaPass</p>
+                </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row row-cols-1 row-cols-md-4 g-4 mt-5">
+            <!-- card1 -->
+            <div class="col text-center">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Halaman Login</h5>
+                    <a href="1_halaman1-login.php" class="btn btn-primary"> Login </a>
+                </div>
+                </div>
+            </div>
+            
+        </div>
+
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
